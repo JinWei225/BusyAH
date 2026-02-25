c482d9a7abbc734732d208555084ec7b7772c9e3
JinWei225 <nefflymicn@gmail.com>
2026-02-25 20:54:59 +0800

    Update mobile UI

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'app.busyah.mobi',
   appName: 'BusyAH',
-  webDir: 'public',
+  webDir: 'out',
   server: {
     url: 'busy-63f5m42zn-jinwei225s-projects.vercel.app',
     cleartext: true
